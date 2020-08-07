03668a4397ab194a6650d3b47d7e4939553a38c3
Thomas Cintra <tcintra@g.hmc.edu>
2020-08-07 23:44:13 +0000

    Completely remodelled personal portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,46 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems, {});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {});
+});
+
+// When the user clicks on the button, scroll to the top of the document
+function goToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function darkMode() {
+  const body = document.getElementsByTagName('body')[0];
+  const file = document.getElementById('file-download');
+  const coursework = document.getElementById('coursework-container');
+  const gallery = document.getElementById('gallery-container');
+  if (body.classList.contains('dark')) {
+    body.classList.remove('dark');
+    coursework.classList.add('lighten-3');
+    coursework.classList.remove('darken-3');
+    file.classList.remove('white-text');
+    file.classList.add('black-text');
+    gallery.classList.add('lighten-3');
+    gallery.classList.remove('darken-3');
+  } else {
+    body.classList.add('dark');
+    coursework.classList.add('darken-3');
+    coursework.classList.remove('lighten-3');
+    file.classList.remove('black-text');
+    file.classList.add('white-text');
+    gallery.classList.remove('lighten-3');
+    gallery.classList.add('darken-3');
+  }
+}
+
 var slideIndex = 1;
 var slideInterval = window.setInterval(function(){
   showSlides();
@@ -28,6 +68,7 @@ function showSlides() {
   var source = "images/gallery"+slideIndex+".jpg";
   myImg.src = source;
   myImg.className = "gallery-img fade";
+  myImg.classList.add("center", "center-align", "center-block");
   gallery.appendChild(myImg);
   gallery.style.textAlign = "center";
 
@@ -48,7 +89,9 @@ function nextSlide() {
 }
 
 function previousSlide() {
-  if (slideIndex <= 1) {
+  if (slideIndex == 2) {
+    slideIndex = 10
+  } else if (slideIndex == 1) {
     slideIndex = 9
   } else {
     slideIndex -= 2;
